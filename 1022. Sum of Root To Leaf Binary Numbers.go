009479c1ec3d42f36356b87aedaff0b1f7e9dc3a
anakin <anakinsun@gmail.com>
2020-02-21 17:31:30 +0800

    Update 1022. Sum of Root To Leaf Binary Numbers.go
    add content of problem

--- 1022. Sum of Root To Leaf Binary Numbers.go
+++ 1022. Sum of Root To Leaf Binary Numbers.go
@@ -1,3 +1,16 @@
+/*
+Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+
+Return the sum of these numbers.
+
+Example 1:
+Input: [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+*/
+
 package main
 
 type TreeNode struct {
